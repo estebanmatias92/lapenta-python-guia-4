47216f9ef313b2f513b21762023d2ad0029d7934
estebanmatias92 <estebanmatias92@gmail.com>
2022-07-13 13:38:09 +0000

    Finished every exercise, ready to upload

--- core/exercises/ejercicio_1.py
+++ core/exercises/ejercicio_1.py
@@ -1,2 +1,44 @@
+print(
+    "1. Cree dos conjuntos de estudiantes, uno para aquellos que rindieron un examen y"
+    + "\notro para aquellos que presentaron un proyecto. Puede usar cadenas para"
+    + "\nrepresentar a los estudiantes, por ejemplo:"
+)
+print("")
 
-print("Este es el ejercicio 1 !!!")
+examenes = {"Andres", "Katy", "Bety", "Emilio", "Susana"}
+proyectos = {"Katy", "Emilio", "Ivan", "Eduardo"}
+
+print("Exámenes:", examenes)
+print("Proyectos:", proyectos)
+
+
+print("\na. ¿Qué estudiantes realizaron el examen y presentaron un proyecto?")
+print(
+    f"Los estudiantes que realizaron el examen y presentaron el proyecto: {examenes.intersection(proyectos)}"
+)
+
+print("")
+print("\n2. ¿Qué estudiantes solo rindieron el examen?")
+print(f"Los estudiantes que solo rindieron el examen: {examenes.difference(proyectos)}")
+
+print("")
+print("3. ¿Qué estudiantes solo presentaron el proyecto?")
+print(
+    f"Los estudiantes que solo presentaron el proyecto: {proyectos.difference(examenes)}"
+)
+
+
+print(
+    "\n4. Haga una lista de todos los estudiantes que tomaron uno (o ambos) del examen y el proyecto."
+)
+print(
+    f"Los estudiantes que tomaron uno (o ambos) del examen y el proyecto: {examenes.union(proyectos)}"
+)
+
+
+print(
+    "\n5. Haga una lista de todos los estudiantes que tomaron cualquiera (pero no ambos) del examen y el proyecto."
+)
+print(
+    f"Los estudiantes que tomaron cualquiera (pero no ambos) del examen y el proyecto: {examenes.symmetric_difference(proyectos)}"
+)
--- core/exercises/ejercicio_2.py
+++ core/exercises/ejercicio_2.py
@@ -1,2 +1,33 @@
+print(
+    "2. Una concordancia es una lista alfabética de las palabras presentes en un texto o"
+    + "\ntextos, con un recuento del número de veces que aparece la palabra."
+    + "\nSu programa de concordancia debe incluir los siguientes pasos:"
+)
+print("")
 
-print("Este es el ejercicio 2 !!!")
+phrase = input("Por favor, ingrese el texto a analizar: ")
+
+print("\nb. Divida la oración en palabras individuales (puede usar split())")
+words = phrase.split(" ")
+print(f"Frase dividida en palabras: {words}")
+
+print(
+    "\nc. Genere una lista de todas las palabras en la oración y la cantidad de veces que ocurren. (counter )."
+)
+
+# Create Dictionary with words and times repeated
+counter = {}
+for word in words:
+    if word not in counter:
+        counter[word] = 0
+    counter[word] += 1
+
+# Show the list of words and the times it is repeated
+for key, value in counter.items():
+    print(f"{key}: {value}")
+
+print("\nd. Produzca una lista alfabética de las palabras con sus cantidades.")
+print("e. Imprima la lista ordenada alfabéticamente.")
+print(
+    f"Lista ordenada alfabeticamente y sus cantidades: {sorted(counter, key = lambda x: x[0])}"
+)
